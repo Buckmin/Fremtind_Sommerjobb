aed7e2d07de7305fba1d0f973d1bb33c61357c88
Buckmin <brynjar1998@hotmail.com>
2020-06-27 08:44:43 +0200

    la til navbar/footer på ruter

--- src/pages/ruter.js
+++ src/pages/ruter.js
@@ -1,12 +1,14 @@
 /* Emma x2 */
 import React from "react"
 import Header from "../components/header"
+import MyFooter from "../components/footer"
 
 
 export default function Home () {
     return (
         <div>
             <Header headerText="ruter header" />
+            <MyFooter />
         </div>
     )
 }
