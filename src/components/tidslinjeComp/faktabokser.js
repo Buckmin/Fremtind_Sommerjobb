eae51d6387b7c85eb61ebeb3becc0c88d1119e6e
Buckmin <brynjar1998@hotmail.com>
2020-07-07 11:27:45 +0200

    ryddet opp tidslinje

---
+++ src/components/tidslinjeComp/faktabokser.js
@@ -0,0 +1,80 @@
+import React from "react"
+import { ManyFunfactsDict } from "./tidslinjeData/funfacts"
+import { totalEmission, userEmissionsSliced, tidslinjePixelHoyde, startDate } from "../tidslinjeComp/totalEmission"
+import { BsArrowLeft } from "react-icons/bs"
+import "../../styles/tidslinje.css"
+
+export function AlleFaktabokser (props) { // denne funksjonen lager et array med alle faktaboksene.
+
+    let boksDict = ManyFunfactsDict() // kg CO2 som key, tekst(er) i array som strings
+    let faktaboksArray = [] // array med faktiske faktabokser. fylles opp i for-løkka nedenfor
+    let boksensYverdi = 0   // y-verdien boksen har fra toppen av diven "tidslinjen"
+    let deciderNumber = 0   // blir satt til random float mellom 0,1 for å bestemme om en fact skal bli vist
+    let dictTekst = ""      // funfact 
+    let arrAvTekster = []   // er flere funfacts per kg CO2, for å gi variasjon
+    let dagerSidenStart = 0    // for å vise dager siden en grense ble passert
+    let currentDate = userEmissionsSliced[0][0]   // for å vise datoen en grense ble passert
+    let emissionSumOnCurrentDate = userEmissionsSliced[0][1] // format: [dato, kg CO2 sluppet ut på dato]
+
+    for( let i = 0; i < totalEmission; i ++) { // i er antall kg CO2 slupper ut
+        deciderNumber = Math.random()
+        if (i > emissionSumOnCurrentDate) { // justerer dato-delen
+            dagerSidenStart ++
+            currentDate = userEmissionsSliced[dagerSidenStart][0]
+            emissionSumOnCurrentDate = emissionSumOnCurrentDate+userEmissionsSliced[dagerSidenStart][1]
+        }
+        if (deciderNumber > 0.80) { // 20% sjanse for å legge ut en boks med gitt funfact
+            boksensYverdi = 10*i
+            if (boksDict[i][1]) { // hvis det eksisterer mer enn element [i][0]
+                arrAvTekster = boksDict[i]
+                 dictTekst = arrAvTekster[Math.floor(Math.random() * arrAvTekster.length)]// velger tilfeldig tekst, dersom det er flere enn en tekst per key
+            }
+            else {dictTekst = boksDict[i]} // når det bare er en value per key
+            faktaboksArray.push(<FaktaBoks 
+                    yVerdi={boksensYverdi} 
+                    boksTekst={dictTekst}   // sender teksten i arrayet til boksen
+                    CO2mengde={ i }
+                    dagenPassert={ dagerSidenStart }
+                    datoPassert={currentDate}
+                    />)
+            i = i + 15 // når er boks er lagt til arrayey, må de neste 15 verdiene hoppes over
+        }
+    }
+    faktaboksArray.push( < YourTotalEmission />) // legger til boks helt nederst på tidslinjen for totalt utslipp
+
+    return (
+      <div id="faktaboksenesBoks" className="div__faktaboksenesBoks" style={{height: tidslinjePixelHoyde}}>
+        {faktaboksArray}
+      </div>
+    )
+  }
+  
+
+  // dette er funksjonen for de enkelete boksene med fakta om utslipp.
+  // props: tekst, mengde CO2, dato passert grense
+  function FaktaBoks (props) {
+    // styling på boksene i 
+    return (
+      <div id="faktaboks" className="div__faktaboksStyle" style ={{top: props.yVerdi}}>
+        <BsArrowLeft /> 
+        <span className="span__CO2tallStyle">{props.CO2mengde}</span> 
+        <span className="span__kgFaktaboksStyle">kg</span>
+        <div id="tekstboks" className="div__tekstboksStyle">
+          <p className="p__datoStyle"> Passert på dag {props.dagenPassert}, som var {props.datoPassert} </p>
+          <p className="p__tekstInBoksStyle">{props.boksTekst}</p>
+          <p className="p__tekstInBoksStyle">{props.currentSum}</p>
+        </div>
+      </div>
+    )
+  }
+  
+  function YourTotalEmission (props) { // dette er boksen nederst på tidslinjen som sier hvor mye totalutslippet vil være
+    return (
+      <div className="div__totalBoksStyle" style={{top: tidslinjePixelHoyde + 100}}> 
+        {/* bare en pil, men nå er boksen for langt nede: <BsArrowLeft />  */}
+        <span className="span__CO2tallStyle">Totalt {totalEmission}</span> 
+        <span className="span__kgFaktaboksStyle">kg</span>
+        <p className="p__tekstInBoksStyle"> sluppet ut gjennom reiser siden {startDate}</p>
+      </div>
+    )
+  }
--- src/pages/profil.js
+++ src/pages/profil.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Header from "../components/header"
 import MyFooter from "../components/footer"
-import { RandomArrDateKgCO2, StringOfRandomArrDateKgCO2 } from "../data/randomdata"
+
 
 
 export default function Home() {
   return ( 
     <div id="hovedkontainer" style={hovedStyle}>
       <Header headerText="profil"/> 
-      {DatoArrayDivs(randomArray)}
+ 
       <MyFooter />
     </div>
   )
@@ -18,18 +18,3 @@ const hovedStyle = {
     maxWidth: "500px",
     margin: "0 auto"
   };
-
-
-
-  // her er koden som viser dato og random CO2-tall
-  const randomArray = StringOfRandomArrDateKgCO2()
-
-
-// denne er for å få hver linje som en div slik at de kommer under hverandre og ikke rett etter hverandre
-  function DatoArrayDivs (arr) {
-    var arrOut = []
-    for (var i = 0; i < arr.length; i++) {
-      arrOut.push(<div>{arr[i]}</div>)
-    }
-    return arrOut
-  }
