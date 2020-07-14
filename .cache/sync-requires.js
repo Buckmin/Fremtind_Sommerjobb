a627a2adde1d2016e82075d88e51f91c584782c3
Buckmin <brynjar1998@hotmail.com>
2020-07-14 11:14:49 +0200

    lagde enkel 404

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/a50501w/Desktop/Gatsby_Projects/fremtind-prototype/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/a50501w/Desktop/Gatsby_Projects/fremtind-prototype/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/a50501w/Desktop/Gatsby_Projects/fremtind-prototype/src/pages/index.js"))),
   "component---src-pages-newroute-js": hot(preferDefault(require("/Users/a50501w/Desktop/Gatsby_Projects/fremtind-prototype/src/pages/newroute.js"))),
   "component---src-pages-profil-js": hot(preferDefault(require("/Users/a50501w/Desktop/Gatsby_Projects/fremtind-prototype/src/pages/profil.js"))),
---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Footer from "../components/footer"
+import Header from "../components/header"
+
+
+
+export default function ErrorPage () {
+    let tekstHeader = "Oi! Her skjedde det noe rart"
+    return (
+        <div style={{textAlign: "center"}}>
+            <Header headerText={tekstHeader} />
+            
+            <h1 style={{fontSize: 150}}>404</h1>
+
+            <img src="https://1.bp.blogspot.com/_wxZUnN00WM4/S3FnhyBGbLI/AAAAAAAABis/-YWbWo_v7Rc/s400/goshDarnIt.png" alt="darn" />
+
+            <Footer />
+        </div>
+    )
+}
