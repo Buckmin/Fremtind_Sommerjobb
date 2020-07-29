59e426ca592e8701e10b0eb68dec3066152715c7
Buckmin <brynjar1998@hotmail.com>
2020-07-29 11:41:11 +0200

    fix: fjernet to testfunksjoner som ikke er i bruk

--- src/pages/tabFolders/favoritter.js
+++ src/pages/tabFolders/favoritter.js
@@ -2,25 +2,6 @@ import React from "react"
 import AccordionJS from "../../components/mineReiser/AccordionJS"
 import {getJson} from "../../getJson";
 
-// gammel testkode
-// function sortByFav(arr) {
-//     const sortedArray = [] // array for cards/AcordionJs greiene
-//     const cardKeyArray = [] // array kun for key i dict alleTurer for å få sortering
-    
-//     for (const key in arr){
-//         if (arr[key]["favoritt"]){ // her blir kun de som er favoritter lagt til. OBS: eneste endringer fra nylige.js
-//             cardKeyArray.push(key)
-//         }
-
-//     }
-//     cardKeyArray.sort() // sortert på dato og reversert for å få nyeste rett under
-//     cardKeyArray.reverse()
-
-//     for (let i = 0; i < cardKeyArray.length; i++){
-//         sortedArray.push(arr[i])
-//     }
-//     return sortedArray
-// }
 
 export default function Favoritter () {
     const alleTurerKopi = getJson("alleTurer") || {};
--- src/pages/tabFolders/nylige.js
+++ src/pages/tabFolders/nylige.js
@@ -2,24 +2,6 @@ import React from "react"
 import AccordionJS from "../../components/mineReiser/AccordionJS"
 import {getJson} from "../../getJson";
 
-// gammel testkode
-// function sortByDate(arr) {
-//     const sortedArray = [] // array for cards/AcordionJs greiene
-//     const cardKeyArray = [] // array kun for key i dict alleTurer for å få sortering
-    
-//     for (const key in arr){
-//         cardKeyArray.push(key)
-//     }
-//     cardKeyArray.sort() // sortert på dato og reversert for å få nyeste rett under
-//     cardKeyArray.reverse()
-
-//     for (let i = 0; i < cardKeyArray.length; i++){
-//         sortedArray.push(arr[i])
-//     }
-//     return sortedArray
-// }
-
-
 export default function Nylige () {
     const alleTurerKopi = getJson("alleTurer") || {};
     //let alleTurerKopi = sortByDate(alleTurer2)
