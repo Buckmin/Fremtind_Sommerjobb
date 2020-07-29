d9e6e24e74d34afbf03947f0f32e218819d5a1e9
Kaja Juel Solheim <kajajs@stud.ntnu.no>
2020-07-29 14:05:05 +0200

    fixed sorting functions, still need fav function

--- src/pages/tabFolders/favoritter.js
+++ src/pages/tabFolders/favoritter.js
@@ -2,27 +2,45 @@ import React from "react"
 import AccordionJS from "../../components/mineReiser/AccordionJS"
 import {getJson} from "../../getJson";
 
+function sortByFav(obj) {
+    const sortedObj = [] // array for cards/AcordionJs greiene
+    const KeyArray = [] // array kun for key i dict alleTurer for å få sortering
+    
+    for (const key in obj){
+        if (obj[key]["favoritt"]){ 
+            KeyArray.push(key)
+        }
+    }
+    
+    KeyArray.sort()
+    KeyArray.reverse()
+
+    for (let i = 0; i < KeyArray.length; i++){
+        sortedObj[KeyArray[i]] = obj[KeyArray[i]]
+    }
+    return sortedObj
+}
 
 export default function Favoritter () {
     const alleTurerKopi = getJson("alleTurer") || {};
-    //let alleTurerKopi = sortByFav(alleTurer2)
+    let alleTurerKopiSorted = sortByFav(alleTurerKopi)
 
     return (
         <div className="container">
-            {Object.keys(alleTurerKopi).map((dato, i) => (
+            {Object.keys(alleTurerKopiSorted).map((dato, i) => (
                 
                 <AccordionJS 
                     key={`${dato.label}${i}`}
                     label={dato.label} 
                     dato={dato} 
-                    fra={alleTurerKopi[dato]['fra']} 
-                    til={alleTurerKopi[dato]['til']} 
-                    lengde={alleTurerKopi[dato]['lengde']} 
-                    tid={alleTurerKopi[dato]['tid']} 
-                    middel={alleTurerKopi[dato]['middel']} 
-                    CO2={alleTurerKopi[dato]['CO2']} 
-                    favoritt={alleTurerKopi[dato]['favoritt']}
-                    kcal = {alleTurerKopi[dato]['kcal'] ? alleTurerKopi[dato]['kcal'] : 0}
+                    fra={alleTurerKopiSorted[dato]['fra']} 
+                    til={alleTurerKopiSorted[dato]['til']} 
+                    lengde={alleTurerKopiSorted[dato]['lengde']} 
+                    tid={alleTurerKopiSorted[dato]['tid']} 
+                    middel={alleTurerKopiSorted[dato]['middel']} 
+                    CO2={alleTurerKopiSorted[dato]['CO2']} 
+                    favoritt={alleTurerKopiSorted[dato]['favoritt']}
+                    kcal = {alleTurerKopiSorted[dato]['kcal'] ? alleTurerKopiSorted[dato]['kcal'] : 0}
                 /> ))}
          </div>
     ) 
--- src/pages/tabFolders/nylige.js
+++ src/pages/tabFolders/nylige.js
@@ -2,25 +2,48 @@ import React from "react"
 import AccordionJS from "../../components/mineReiser/AccordionJS"
 import {getJson} from "../../getJson";
 
+function sortByDate(obj) {
+    const sortedObj = [] // array for cards/AcordionJs greiene
+    const KeyArray = [] // array kun for key i dict alleTurer for å få sortering
+    
+    for (const key in obj){
+        KeyArray.push(key)
+    }
+    
+    KeyArray.sort()
+    KeyArray.reverse()
+
+    for (let i = 0; i < KeyArray.length; i++){
+        sortedObj[KeyArray[i]] = obj[KeyArray[i]]
+        
+    }
+    return sortedObj
+}
+
 export default function Nylige () {
+    
     const alleTurerKopi = getJson("alleTurer") || {};
-    //let alleTurerKopi = sortByDate(alleTurer2)
+    let alleTurerKopiSorted = sortByDate(alleTurerKopi)
+
+    console.log('alleTurerKopiSorted: ', alleTurerKopiSorted)
+    console.log('alleTurerKopi: ', alleTurerKopi)
+
 
     return (
         <div className="container">
-            {Object.keys(alleTurerKopi).map((dato, i) => (
+            {Object.keys(alleTurerKopiSorted).map((dato, i) => (
                 <AccordionJS 
                     key={`${dato.label}${i}`}
                     label={dato.label}
                     dato={dato} 
-                    fra={alleTurerKopi[dato]['fra']} 
-                    til={alleTurerKopi[dato]['til']} 
-                    lengde={alleTurerKopi[dato]['lengde']} 
-                    tid={alleTurerKopi[dato]['tid']} 
-                    middel={alleTurerKopi[dato]['middel']} 
-                    CO2={alleTurerKopi[dato]['CO2']} 
-                    favoritt={alleTurerKopi[dato]['favoritt']}
-                    kcal = {alleTurerKopi[dato]['kcal'] ? alleTurerKopi[dato]['kcal'] : 0}
+                    fra={alleTurerKopiSorted[dato]['fra']} 
+                    til={alleTurerKopiSorted[dato]['til']} 
+                    lengde={alleTurerKopiSorted[dato]['lengde']} 
+                    tid={alleTurerKopiSorted[dato]['tid']} 
+                    middel={alleTurerKopiSorted[dato]['middel']} 
+                    CO2={alleTurerKopiSorted[dato]['CO2']} 
+                    favoritt={alleTurerKopiSorted[dato]['favoritt']}
+                    kcal = {alleTurerKopiSorted[dato]['kcal'] ? alleTurerKopiSorted[dato]['kcal'] : 0}
                 /> ))}
         </div>
     ) 
